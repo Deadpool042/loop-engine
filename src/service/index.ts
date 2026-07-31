28f921c6cd38a48b04da622ea2c485ad0a507732
Laurent <90608631+Deadpool042@users.noreply.github.com>
2026-07-31 11:32:14 +0200

    feat(service): export deployable service application

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,3 +1,5 @@
 export * from "./orchestration-service-lifecycle.js";
 export * from "./orchestration-service-transport.js";
 export * from "./node-http-service-adapter.js";
+export * from "./orchestration-service-configuration.js";
+export * from "./orchestration-service-application.js";
