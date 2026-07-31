79c2d895777fcc2c680297c9dbb8757c006b7f43
Laurent <90608631+Deadpool042@users.noreply.github.com>
2026-07-31 11:43:07 +0200

    feat(service): export HMAC authentication boundary

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,3 +3,4 @@ export * from "./orchestration-service-transport.js";
 export * from "./node-http-service-adapter.js";
 export * from "./orchestration-service-configuration.js";
 export * from "./orchestration-service-application.js";
+export * from "./orchestration-service-authentication.js";
