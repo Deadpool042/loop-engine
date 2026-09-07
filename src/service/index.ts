c2c6dcf79d5e30c79a8c9ed86fcd5ca11a8c6757
Laurent <90608631+Deadpool042@users.noreply.github.com>
2026-09-07 20:13:53 +0200

    chore(cleanup): remove orphan service and audit wiring

--- src/service/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./orchestration-service-lifecycle.js";
-export * from "./orchestration-service-transport.js";
-export * from "./node-http-service-adapter.js";
-export * from "./orchestration-service-configuration.js";
-export * from "./orchestration-service-application.js";
-export * from "./orchestration-service-authentication.js";
-export * from "./orchestration-service-persistent-auth-store.js";
